3b2784831cba25f3945f54285115c3f69fcd8b98
Mubarek Shikur <manmuba90@gmail.com>
2023-09-09 16:44:44 +0300

    nav

--- app/Components/Navigation/NavigationBar.tsx
+++ app/Components/Navigation/NavigationBar.tsx
@@ -1,14 +1,14 @@
 "use client";
 
 import React, { useState } from "react";
-import { IoWallet } from 'react-icons/io5'
+import { IoWallet } from "react-icons/io5";
 import Logo from "../Logo";
 import SearchInput from "../Input";
 
 const NavigationBar = () => {
   const [isDark, setIsDark] = useState(true);
   return (
-    <div className="w-full h-20 fixed flex flex-row justify-around items-center border-b-2">
+    <div className="w-full h-20 fixed flex flex-row justify-between items-center border-b-2 pl-5 pr-5 ">
       <div className="flex flex-row lg:gap-8 md:gap-6">
         <Logo first="Bita" second="kon" isDark={isDark} />
         <SearchInput />
@@ -20,10 +20,11 @@ const NavigationBar = () => {
       </div>
       <div className="flex">
         <div className="rounded-full bg-blue-500 flex flex-row items-center justify-center gap-2 p-2 pl-5 pr-5 text-opacity-100 text-white">
-        <IoWallet />
-        <span>Wallet</span>
+          <IoWallet />
+          <span>Wallet</span>
         </div>
       </div>
+      
     </div>
   );
 };
