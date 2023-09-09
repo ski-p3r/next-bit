a4bf673fdd542107bf94ab46681636b4e948a750
Mubarek Shikur <manmuba90@gmail.com>
2023-09-09 15:53:05 +0300

    Internal Dark

--- app/Components/Logo.tsx
+++ app/Components/Logo.tsx
@@ -3,16 +3,18 @@ import React from 'react'
 interface LogoProps {
     first: string;
     second: string;
+    isDark: boolean;
 }
 
 const Logo: React.FC<LogoProps> = ({
     first,
-    second
+    second,
+    isDark
 }) => {
   return (
-    <div className='flex flex-row items-center justify-center font-extrabold'>
-        <h4 className='text-black'>{first}</h4>
-        <h4 className='text-blue-700'>{second}</h4>
+    <div className='flex flex-row items-center justify-center font-extrabold font-xl'>
+        <h4 className={isDark ? 'text-green-500 text-[20px]' : 'text-black text-[20px]'}>{first}</h4>
+        <h4 className='text-blue-700 text-[20px]'>{second}</h4>
     </div>
   )
 }
--- app/Components/Navigation/NavigationBar.tsx
+++ app/Components/Navigation/NavigationBar.tsx
@@ -1,15 +1,16 @@
-import React from 'react'
-import Logo from '../Logo'
+"use client";
+
+import React, { useState } from "react";
+import Logo from "../Logo";
 
 const NavigationBar = () => {
+  const [isDark, setIsDark] = useState(true);
   return (
-    <div className='w-full bg-white h-20 fixed flex flex-col justify-around items-center'>
-        <Logo 
-            first='Bita'
-            second='kon'
-        />
+    <div className="w-full bg-white h-20 fixed flex flex-col justify-around items-center">
+      <Logo first="Bita" second="kon" isDark={isDark} />
+      
     </div>
-  )
-}
+  );
+};
 
-export default NavigationBar
+export default NavigationBar;
